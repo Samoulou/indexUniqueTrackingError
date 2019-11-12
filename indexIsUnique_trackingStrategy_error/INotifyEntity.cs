7b5ba6398e19b272698870c01dac6b1f5f0ff434
Samuel Coppey <Samuel.Coppey@polyright.com>
2019-11-12 08:04:24 +0100

    tracking error

---
+++ indexIsUnique_trackingStrategy_error/INotifyEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace indexIsUnique_trackingStrategy_error
+{
+    public class INotifyEntity : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        protected void SetWithNotify<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (!Equals(field, value))
+            {
+                PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+                field = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ indexIsUnique_trackingStrategy_error/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace indexIsUnique_trackingStrategy_error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new CoreContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                var product = new Product
+                {
+                    Name = "Product 1",
+                };
+                db.Products.Add(product);
+                db.SaveChanges();
+                System.Threading.Thread.Sleep(5000);
+                db.Products.Remove(product);
+                db.SaveChanges();
+            }
+        }
+
+        public class CoreContext : DbContext
+        {
+            public DbSet<Product> Products { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=testApp;Trusted_Connection=True;");
+                optionsBuilder.ConfigureWarnings(b =>
+                {
+                    b.Default(WarningBehavior.Throw);
+                    b.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.CoreEventId.SensitiveDataLoggingEnabledWarning);
+                });
+                optionsBuilder.EnableSensitiveDataLogging();
+                optionsBuilder.UseLoggerFactory(new Microsoft.Extensions.Logging.LoggerFactory(new[] { new Microsoft.Extensions.Logging.Debug.DebugLoggerProvider() }));
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotifications);
+                modelBuilder.Entity<Product>(entity =>
+                {
+                    entity.HasKey(e => e.ProductId);
+                    entity.HasIndex(e => e.BarCode).IsUnique();
+                    entity.Property(e => e.ProductId).ValueGeneratedOnAdd();
+                });
+            }
+        }
+
+        public class Product : INotifyEntity
+        {
+            private Guid _productId;
+            private string _name;
+            private int _barCode;
+
+            public Guid ProductId
+            {
+                get => _productId;
+                set => _productId = value;
+            }
+
+            public string Name
+            {
+                get => _name;
+                set => _name = value;
+            }
+
+            public int BarCode
+            {
+                get => _barCode;
+                set => _barCode = value;
+            }
+        }
+    }
+}
